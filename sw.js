26ccff152b6becbdef4cbeee7c484d103f77bc7c
Gigakox3534 <g.szymon190@gmail.com>
2026-09-12 16:17:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'dziennik-v1';
+const CACHE = 'dziennik-v3';
 const SHELL = ['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-maskable-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install', e => {
